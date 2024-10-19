509e86c91788dd051ca8c68e03ffa06f22e2dffa
Shristi Verma <shristiverma1311@gmail.com>
2024-10-19 20:29:42 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
-import './App.css';
+
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-2xl font-bold text-green-600">
-      hi all, let's build netflix gpt
-    </div>
+    <Body />
   );
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Login from './Login'
+import Browse from './Browse'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: "/browse",
+            element: <Browse />
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ]);
+
+    return (
+        <div>
+            <RouterProvider router={appRouter} />
+            
+        </div>
+    )
+}
+
+export default Body
